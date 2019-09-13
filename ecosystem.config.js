7aaae77d7beb0394ba8e889f84c7c3ba2a124d90
geraldanosike <geraldanosike.javhse@gmail.com>
2019-09-13 18:49:23 +0100

    pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,36 @@
+// module.exports = {
+//   apps: [
+//     {
+   
+//       script: "npm",
+//       args: "start"
+//     }
+//   ]
+// };
+
+
+
 module.exports = {
   apps: [
     {
       name: "aws-node-deploy",
       script: "npm",
-      args: "start"
+      env: {
+        PORT: 3000,
+        NODE_ENV: "production"
+      }
     }
-  ]
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "ec2-18-221-52-202.us-east-2.compute.amazonaws.com",
+      // key: "~/.ssh/node-app.pem",
+      key: "~/documents/vgg-learn/node-app.pem",
+      ref: "origin/master",
+      repo: "https://github.com/geraldanosike/aws-node-deploy.git",
+      path: "/home/ubuntu/aws-node-deploy",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+    }
+  }
 };
