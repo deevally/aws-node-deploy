9c1b63a47cc56f5612a47017214f27f8f11b6fa7
geraldanosike <geraldanosike.javhse@gmail.com>
2019-09-30 14:11:50 +0100

    deploy script edited

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,37 +1,37 @@
-// module.exports = {
-//   apps: [
-//     {
-   
-//       script: "npm",
-//       args: "start"
-//     }
-//   ]
-// };
-
-
-
 module.exports = {
   apps: [
     {
       name: "aws-node-deploy",
-      // script: "index.js",
       script: "npm",
-      args: "start",
-      env: {
-        PORT: 3000,
-        NODE_ENV: "production"
-      }
+      args: "start"
     }
-  ],
-  deploy: {
-    production: {
-      user: "ubuntu",
-      host: "ec2-18-221-52-202.us-east-2.compute.amazonaws.com",
-      key: "~/documents/vgg-learn/node-app.pem",
-      ref: "origin/master",
-      repo: "https://github.com/geraldanosike/aws-node-deploy.git",
-      path: "/home/ubuntu/aws-node-deploy",
-      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
-    }
-  }
+  ]
 };
+
+
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: "aws-node-deploy",
+//       // script: "index.js",
+//       script: "npm",
+//       args: "start",
+//       env: {
+//         PORT: 3000,
+//         NODE_ENV: "production"
+//       }
+//     }
+//   ],
+//   deploy: {
+//     production: {
+//       user: "ubuntu",
+//       host: "ec2-18-221-52-202.us-east-2.compute.amazonaws.com",
+//       key: "~/documents/vgg-learn/node-app.pem",
+//       ref: "origin/master",
+//       repo: "https://github.com/geraldanosike/aws-node-deploy.git",
+//       path: "/home/ubuntu/aws-node-deploy",
+//       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
+//     }
+//   }
+// };
